178fd54d4ea53425ebc5fc717cd4fb61ecfec011
ALAMEEN <almvzm@gmail.com>
2025-06-14 01:54:33 +0530

    Add files via upload

---
+++ Kseb.java
@@ -0,0 +1,60 @@
+public class Kseb
+{
+	public static void main(String args[])
+	{
+		System.out.print("\n\n\n\n");
+		
+		System.out.print("Electricity Bills||Electricity BOARD|KSEB\n");
+		System.out.print("KSEBVIZHINJAM\n");
+		System.out.print("22345|\n,ksebvzm@gmail.com\nwww.ksebvzm.com");
+		System.out.print("\n\n\n\n\n\n\n");
+		System.out.print("*Bill Summary\n");
+		System.out.print("--------------\n");
+		
+		System.out.print("\t\t* Detail\t\tInformation\n");
+		System.out.print("\t\t* Bill Number   \t\tEbi2345\n");
+		System.out.print("\t\t* Bill Date     \t13-june-25\n");
+		System.out.print("\t\t* Due Date\t\t27-june-25\n");
+		System.out.print("\t\t* Consumer Number\t\t98765478\n");
+		System.out.print("\t\t* Meter No\t\tMT45678\n");
+		System.out.print("\t\t* Billing Period\t01-may-25to31-MAY-25\n");
+		System.out.print("\t\t* Consumer Name\t\t Al Ameen\n");
+		System.out.print("\t\t* Address\t\t123bird street,mumbai\n\n\n\n");
+		System.out.print("METER DETAILS\n");
+		System.out.print("-------------\n");
+		System.out.print("\t\t* Parameter\t\tvalue\n");
+		System.out.print("\t\t* Previous Reading\t\t12450\n");
+		System.out.print("\t\t* Current Reading\t\t12870\n");
+		System.out.print("\t\t* Unit Readind\t\t420KWH\n\n\n\n");
+		System.out.print("charges\n");
+		System.out.print("-------\n");
+		System.out.print("\t\t* Description\t\tAmount($)\n");
+		System.out.print("\t\t* Energy Charge\t\t2,100.00\n");
+		System.out.print("\t\t* Fixed Charge\t\t100.00\n");
+		System.out.print("\t\t* Meter Rate\t\t20.00\n");
+		System.out.print("\t\t* Taxes Duty\t\t75.00\n");
+		System.out.print("\t\t* Total Amount Due\t$2,295.00\n\n\n\n");
+		System.out.print("PAYMENT INFO\n");
+		System.out.print("------------\n");
+		System.out.print("\t\t*Last Payment Date 15-May-2025\n");
+		System.out.print("\t\t*Last Payment Amount$2.030.00\n");
+		System.out.print("\t\t*Mode Of Payment:UPI\n\n\n\n");
+		System.out.print("NOTES\n");
+		System.out.print("-----\n");
+		
+		System.out.print("\t\t* Pay before the due to avoid disconnection.\n");
+		System.out.print("\t\t* Late fee of $50 applies the due date\n ");
+		System.out.print("\t\t* For enquires call[223456]\n");
+		System.out.print("================================================");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
