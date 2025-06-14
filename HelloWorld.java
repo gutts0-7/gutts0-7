a2d235bc4f6d0d9255bc911efd7f2b48633af09e
ALAMEEN <almvzm@gmail.com>
2025-06-14 13:06:48 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld{
+	public static void main(String args[]){
+
+		/*String msg = "How are you";
+		String name = "Sha";
+
+		System.out.println("Hi "+name+", "+msg);*/
+
+		//two numbers and its sum
+		int n1=10, n2 =3;
+		int sum = n1 + n2;
+		int sub = n1 - n2;
+
+		//showing sum of two numbers in screen
+		System.out.println("Sum of "+ n1 + " and "+ n2 +" is " + sum);
+		System.out.println("Sub of "+ n1 + " and "+ n2 +" is " + sub);
+
+		System.out.println("Mul of "+ n1 + " and "+ n2 +" is " + (n1 * n2));
+		System.out.println("Div of "+ n1 + " and "+ n2 +" is " + (n1 / n2));
+
+		//modulare 
+		int mod = n1 % n2;
+		System.out.println("Mod of "+ n1 + " and "+ n2 +" is " + mod);
+
+
+
+
+	}
+}
